defda0712e9be3887ff1e5069a8af083e0464a00
jsd0030 <114434389+jsd0030@users.noreply.github.com>
2022-10-12 22:50:36 -0500

    Add files via upload

---
+++ Dickey_HeyDay.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#this is the shebang line to start off the script
+echo "Hey! Type your name:"
+#user will type their name because my echo command tells them to
+read
+echo "Hey $REPLY! Today is Hey Day at Auburn University. War Eagle!"
+#that echo command takes their input and puts it into the response so I can say hey to them!
+
